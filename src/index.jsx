1d17ff77db15104a214334d3ab8ed9788da78e34
Gerald Lee <gerald_4231@hotmail.com>
2022-10-11 23:48:57 +0800

    vertial

--- src/index.jsx
+++ src/index.jsx
@@ -15,6 +15,7 @@ import RolePlanner from './pages/RolePlanner.jsx'
 import LearingJourneySkills from './pages/LearningJourneySkills'
 import LearningJourneyCourses from "./pages/LearningJourneyCourses"
 import SuccessPage from "./pages/SuccessPage"
+import SkillVerticalTimeline from "./pages/SkillVerticalTimeline"
 
 ReactDOM.render(
     <React.StrictMode>
@@ -23,6 +24,7 @@ ReactDOM.render(
                     <Route path="/" element={<Login />}></Route>
                     <Route use exact path="/staffhomepage"element={<StaffHomePage />}></Route>
                     <Route path="/staffhomepage/:roleid"element={<LearingJourneySkills />}></Route>
+                    {/* <Route path="/staffhomepage/:roleid"element={<SkillVerticalTimeline />}></Route> */}
                     <Route path="/staffhomepage/:roleid/:skillid"element={<LearningJourneyCourses />}></Route>
                     <Route path="/staffrolepage/" element={<StaffRolePage />}></Route>
                     <Route path="/staffskillpage/" element={<StaffSkillPage />}></Route>
---
+++ src/pages/SkillVerticalTimeline.jsx
@@ -0,0 +1,172 @@
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
+import StaffSideBar from "../components/StaffSideBar"
+import SearchInput from "../components/SearchInput"
+import React, { useContext } from "react";
+import SkillCard from '../components/SkillCard'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useParams
+} from "react-router-dom";
+import '../index.css'
+const LearningJourneySkills = () => {
+
+    //Fetch All Skills
+    var skillData = {
+        "data": {
+            '1': {
+                "Skill_ID": '1',
+                "Skill_name": "Management",
+                "Skill_courses": ['COR001', 'tch018', 'tch012', 'MGT002', 'MGT003']
+            },
+            '2': {
+                "Skill_ID": '2',
+                "Skill_name": "IT",
+                "Skill_courses": ['COR001', 'tch019', 'HRD001', 'tch009', 'tch013', 'MGT004']
+            },
+            '3': {
+                "Skill_ID": '3',
+                "Skill_name": "Operations",
+                "Skill_courses": ['COR001', 'tch006', 'tch001', 'tch018']
+            },
+            '4': {
+                "Skill_ID": '4',
+                "Skill_name": "Data Analytics",
+                "Skill_courses": ['COR001', 'FIN001', 'MGT001']
+            },
+            '5': {
+                "Skill_ID": '5',
+                "Skill_name": "Digital Leadership",
+                "Skill_courses": ['COR001', 'FIN001', 'MGT001', 'tch005', 'tch009']
+            }
+        }
+    }
+
+    //Fetch Staff data By ID
+    var staffData = {
+        "data": {
+            "Dept": "Sales",
+            "Role": "2",
+            "Email": "jack.sim@allinone.com.sg",
+            "Learning_Journeys": [
+                {
+                    "Role_ID": '1',
+                    "Role_name": "Web Developer",
+                    "Skills": ['1', '2', '3', '4'],
+                    "Courses": ['COR001', 'tch019', 'HRD001', 'tch009', 'tch013', 'MGT004'],
+                    "Progress": 'Completed'
+                },
+                {
+                    "Role_ID": '2',
+                    "Role_name": "Frontend Engineer",
+                    "Skills": ['1', '2', '3', '4', '5'],
+                    "Courses": ['COR001', 'tch019', 'HRD001', 'tch009', 'tch013', 'MGT004'],
+                    "Progress": 'Completed'
+                },
+                {
+                    "Role_ID": '3',
+                    "Role_name": "Backend Engineer",
+                    "Skills": ['1', '2', '3', '4', '5'],
+                    "Courses": ['COR001', 'tch019', 'HRD001', 'tch009', 'tch013', 'MGT004'],
+                    "Progress": 'Completed'
+                },
+                {
+                    "Role_ID": '4',
+                    "Role_name": "Caleb Insulter",
+                    "Skills": ['1', '2', '3', '4'],
+                    "Courses": ['COR001', 'tch019', 'HRD001', 'tch009', 'tch013', 'MGT004'],
+                    "Progress": 'Ongoing'
+                },
+            ],
+            "Completed_Courses": ['COR001'],
+            "Completed_Skills": ['1', '2'],
+            "Staff_FName": "John",
+            "Staff_ID": '130001',
+            "Staff_LName": "Sim"
+        }
+    }
+
+    //user's learning journeys
+    const userLearningJourneys = staffData.data['Learning_Journeys']
+
+    let { roleid } = useParams()
+    console.log(roleid)
+    let journey = null
+    for (let obj of userLearningJourneys) {
+        if (obj['Role_ID'] == roleid) {
+            journey = obj
+        }
+    }
+
+    const skillCards = journey['Skills'].map(function (skillid) {
+        if (staffData.data['Completed_Skills'].includes(skillid)) {
+            return <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                skillid={skillid} roleid={journey['Role_ID']} completed={true}
+            >
+                <h3 className="vertical-timeline-element-title">{skillData.data[skillid]['Skill_name']}</h3>
+                <h4 className="vertical-timeline-element-subtitle">{skillData.data[skillid]['Skill_courses'].length} Courses</h4>
+                <p>
+                    Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+                </p>
+            </VerticalTimelineElement>
+        }
+        else {
+            return <VerticalTimelineElement
+                className="vertical-timeline-element--work m-4"
+                contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                skillid={skillid} roleid={journey['Role_ID']} completed={false}
+            >
+                <h3 className="vertical-timeline-element-title">{skillData.data[skillid]['Skill_name']}</h3>
+                <h4 className="vertical-timeline-element-subtitle">{skillData.data[skillid]['Skill_courses'].length} Courses</h4>
+                <p>
+                    Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+                </p>
+            </VerticalTimelineElement>
+        }
+
+    })
+
+
+
+    return (
+        <div className="container-full grid grid-cols-4 grid-rows-6 max-h-screen">
+            <div className="col-span-1">
+                <StaffSideBar select='Learning Journey' />
+            </div>
+
+            <div className='col-start-2 col-end-4 my-auto'>
+                <h1 className="text-3xl font-medium text-start ml-12">
+                    Learning Journey - <span className='font-bold'>{journey['Role_name']}</span>
+                </h1>
+            </div>
+
+            <div className='col-end-5 col-span-1 my-auto mx-12'>
+                <SearchInput />
+            </div>
+            <div className='col-start-2 col-end-5 row-start-2 row-end-6 rounded-lg overflow-y-auto max-h-screen mx-12'>
+                <h1 className='font-bold text-2xl sticky top-0 w-full text-center bg-white'>{journey['Skills'].length} Skills Required</h1>
+                <div className='grid grid-cols-1 p-6 gap-6'>
+
+                    <VerticalTimeline className='rounded-lg'>
+
+                        {skillCards}
+                    </VerticalTimeline>
+
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default LearningJourneySkills

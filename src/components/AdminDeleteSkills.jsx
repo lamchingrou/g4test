b9c4aa2941c677386ccb7a5896d4ffbdcd8177f5
Lam Ching Rou <crlam.2020@smu.edu.sg>
2022-10-12 20:09:33 +0800

    change link to button in admin add and delete role's skills

--- src/components/AdminDeleteSkills.jsx
+++ src/components/AdminDeleteSkills.jsx
@@ -34,10 +34,10 @@ export default function AdminDeleteSkills() {
         return <tr className="hover:bg-white-200">
             <td className="whitespace-nowrap py-2 font-medium text-gray-900 text-center">{item['Skill_name']}</td>
             <td className="whitespace-nowrap px-4 py-2 text-gray-700 text-center">
-                <a className="inline-flex items-center rounded-2xl border border-gray-600 bg-gray-600 px-4 py-2 text-white hover:bg-transparent hover:text-gray-600 focus:outline-none focus:ring active:text-gray-500"
-                    href={"/admindeleteskill/"+item['Skill_ID']}>
+                <button className="inline-flex items-center rounded-2xl border border-gray-600 bg-gray-600 px-4 py-2 text-white hover:bg-transparent hover:text-gray-600 focus:outline-none focus:ring active:text-gray-500">
+                    {/* // href={"/admindeleteskill/"+item['Skill_ID']}> */}
                     <span className="text-xs font-medium"> Delete Skill </span>
-                </a>
+                </button>
             </td>
         </tr>
 

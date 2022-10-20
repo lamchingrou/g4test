7c4d9aae7c4409447fa1f6d2b03f4f6857b0e117
Gerald Lee <gerald_4231@hotmail.com>
2022-10-20 19:50:06 +0800

    admincreaterole

--- src/pages/AdminCreateRole.jsx
+++ src/pages/AdminCreateRole.jsx
@@ -3,8 +3,33 @@ import '../index.css'
 import AdminSideBar from '../components/AdminSideBar'
 import SearchInput from '../components/SearchInput'
 import AdminUpdateSkill from "../components/AdminUpdateSkill"
-
+import { AddedContext } from '../components/RolePlannerContext'
+import { useState } from 'react'
+import { skillData, roleData } from '../global.js'
 export default function AdminCreateRole() {
+    function handleNameChange(event){
+        setRoleNaame(event.target.value)
+        console.log(roleName)
+    }
+    function processCreateRole(){
+        if(added[0].length == 0 || roleName.length == 0){
+            console.log('fail') //fail cos no skills at all
+        }
+        else{
+            const jobName = roleName
+            const editedSkills = added[0] //update the role
+            //fetch all jobs
+            //Job_ID = jobs.length
+             
+        }
+    }
+    var addedSkills = []
+    var unaddedSkills = []
+    for (let key of Object.keys(skillData.data)) {
+        unaddedSkills.push(skillData.data[key])
+    }
+    const [roleName, setRoleNaame] = useState('')
+    const [added, setAdded] = useState([addedSkills, unaddedSkills]) //Default is [Job's Added Skills, Job's UnAdded Skills]
     return (
         <div className="container-full grid grid-cols-5 grid-rows-6 max-h-screen">
             <div className="col-span-1">
@@ -17,13 +42,14 @@ export default function AdminCreateRole() {
                 </h1>
                 <div class="ml-4">
                     <label class="sr-only"> Role Name </label>
-                    <input type="search" id="roleName" placeholder="Role Name" class="w-full rounded-md border border-gray-200 shadow-sm sm:text-sm p-3" />
+                    <input onChange={handleNameChange} type="search" id="roleName" placeholder="Role Name" class="w-full rounded-md border border-gray-200 shadow-sm sm:text-sm p-3" />
                 </div>
             </div>
            
             <div className='col-start-4 col-end-6 col-span-2 my-auto mx-12'>
                 <SearchInput />
             </div>
+            <AddedContext.Provider value={{ added, setAdded }}>
             <div className='col-start-2 col-end-4 row-start-2 row-end-6 border rounded-lg overflow-y-auto max-h-screen mx-12'>
                 <table class="min-w-full divide-y-2 divide-gray-200 text-sm relative">
                     <thead>
@@ -45,6 +71,14 @@ export default function AdminCreateRole() {
                     </thead>
                     <AdminUpdateSkill action='add' />
                 </table>
+            </div>
+            </AddedContext.Provider>
+            <div className='col-start-4 text-end col-end-6 row-start-6 row-end-6 my-auto overflow-y-auto max-h-screen mx-12'>
+                <a  onClick={()=>processCreateRole()}
+                    class="inline-block rounded-lg border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+                    href="#">
+                    Create Role
+                </a>
             </div>
         </div >
 
